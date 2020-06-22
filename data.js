d8787424cea98abd0ca101586c73b0787ca16476
Hemasriya <66292187+Hemasriya@users.noreply.github.com>
2020-06-22 08:55:49 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,81 @@
+{
+  "profiles":[
+    {
+      "basics":{
+        "name":"Hema Sriya",
+        "role":"Student",
+        "email":"hemasriya369@gmail.com",
+        "mobile":"+91 8688916733",
+        "gender":"female",
+        "address":"mallela,Andhra pradesh"
+
+      },
+      "summery":[
+        "I am Fresher in computer science engineering",
+                "But i having knowledge in some technologies like HTML5,CSS3,and Javascript",
+                "Having knowledge on Python, C++,React",
+                "Used different kinds of frameworks for backend i.e Codeigniter,Django and Express",
+                "Used databases like mySql,oracle and firebase"
+
+      ],
+      "education":[
+        {
+                    "degree":"B-Tech (Computer Science and Engineering)",
+                    "institute":"Lakireddy balireddy College of Engineering",
+                    "result":"95%"
+        },
+        {
+                    "degree":"Intermediate in MPC",
+                    "institute":"Sri Chaitanya Junior College",
+                    "result":"96%"
+        },
+        {
+                    "degree":"S.S.C",
+                    "institute":"Vignan Global Gen School",
+                    "result":"10/10"
+        }
+
+
+      ]
+    },
+      {
+        "basics":{
+        "name":"Sai",
+        "role":"Student",
+        "email":"saimandava@gmail.com",
+        "mobile":"+91 7993619983",
+        "gender":"female",
+        "address":"gudivada,Andhra pradesh"
+
+      },
+      "summery":[
+        "I am Fresher in computer science engineering",
+                "But i having knowledge in some technologies like HTML5,CSS3,and Javascript",
+                "Having knowledge on Python, C++,React",
+                "Used different kinds of frameworks for backend i.e ,Django and Express",
+                "Used databases like mySql,oracle"
+
+      ],
+      "education":[
+        {
+                    "degree":"B-Tech (Computer Science and Engineering)",
+                    "institute":"Lakireddy balireddy College of Engineering",
+                    "result":"96%"
+        },
+        {
+                    "degree":"Intermediate in MPC",
+                    "institute":"Sri Chaitanya Junior College",
+                    "result":"95%"
+        },
+        {
+                    "degree":"S.S.C",
+                    "institute":" sri Chaitanya school",
+                    "result":"95%"
+        }
+
+
+      ]
+      }
+    
+  ]
+}
